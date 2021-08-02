900c8f64dfc3ec25ac2c09522f7734a6da7325e0
leejojosepj <leejojosepj@gmail.com>
2021-08-02 21:28:57 +0530

    Updates

---
+++ precache-manifest.048eb91a6c47eebcd4bd6d50bc753b7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97b037e8f045f2675281dae955caf6d",
+    "url": "/react-app-dip/index.html"
+  },
+  {
+    "revision": "107e2b6aca6268e8e175",
+    "url": "/react-app-dip/static/css/main.ab5a0fcb.chunk.css"
+  },
+  {
+    "revision": "073b321df77737db16f0",
+    "url": "/react-app-dip/static/js/2.ad3588c6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-app-dip/static/js/2.ad3588c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107e2b6aca6268e8e175",
+    "url": "/react-app-dip/static/js/main.e04ed565.chunk.js"
+  },
+  {
+    "revision": "39738fe3751cbefbef09",
+    "url": "/react-app-dip/static/js/runtime-main.3ca7572f.js"
+  }
+]);
